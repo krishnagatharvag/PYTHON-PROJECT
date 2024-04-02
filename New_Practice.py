58b20744515de986c4f573ceda2f224ebb3580d1
krishna <krishnakantg76@gmail.com>
2024-04-02 10:35:32 +0530

    new practice were updated

--- New_Practice.py
+++ New_Practice.py
@@ -1,1 +1,62 @@
-print("Hellow I have created new file")
+
+#swap two number
+#odd even number
+#Palindrome
+#cheking whether the given number is prime or not
+#prime number in given interval
+#fibonacci series
+#create a new list having no duplicate(no repetation) value from two list
+# create a new list having unique values from a given list
+# create a new list having duplicate values from a given list
+# create a new list having duplicate values from two list
+#store the qube values of a list in list using for loop
+#reverse the number
+# reverse the string
+#reverse the string withowt slicing
+#reverse the string withowt slicing using list
+#reverse the string with string variable
+# reverse the words of the string
+# reverse the indivisual words of the string
+# reverse the indivisual words of the string using list
+# random and randit function
+#count function
+# counter librery
+# counter in sorted order
+#find the occarence of an element in a list or string
+#swap first and last element of string
+#swap any two elements of list
+#deleting element from a list
+#slicing for avoiding number
+#list comprehension
+# squre of a given list
+#convert list into string
+#table of two
+#search an element in a list
+#tuple packing unpaking
+# reverse the number using while loop
+#string with no duplicate value
+#replace function
+#sorting
+#sorting by alphabet
+#membership operator
+#create a dictonary from two given lists
+#extended function
+# chenge the required charactor to uppr case latter
+# input a4b3c2d1 output aaaabbbcca
+# input d1 outpuc2b3a4t aaaabbbcca(shorted order)
+#input aaaabbbccd outpot a4b3c2a1
+# extract word starting letter with k in s string/list
+# adding elements of two list
+#adding elements of list
+#find out common letters in between two strings
+#Pattern star
+
+
+
+# input "abc" output "bcd"
+# should only accept  first 4 charactor then 3 number then 2 charactor
+
+
+
+
+
